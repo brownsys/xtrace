6b86d99a52b5094831c90a94225b4c3e908048bc
Jonathan Mace <jonathan.c.mace@gmail.com>
2014-12-12 19:13:23 -0500

    Tweak to graph viz - names were incorrect on nodes

--- src/main/resources/webui/html/interactive/js/DirectedAcyclicGraph.js
+++ src/main/resources/webui/html/interactive/js/DirectedAcyclicGraph.js
@@ -73,7 +73,7 @@ function DirectedAcyclicGraph() {
     var height = d3.functor("100%");
     var edgeid = function(d) { return d.source.id + d.target.id; }
     var nodeid = function(d) { return d.id; }
-    var nodename = function(d) { return d.report["Agent"] ? d.report["Agent"][0] : ""; }
+    var nodename = function(d) { return d.report["Agent"] ? d.report["Agent"].substr(-25) : ""; }
     var getnodes = function(d) { return d.getVisibleNodes(); }
     var getedges = function(d) { return d.getVisibleLinks(); }
     var bbox = function(d) {
